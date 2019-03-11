e0415ebf26623bb3d9e89e099132385673a91a5e
CLAUDIO GOMES <claudio.silva@fnde.gov.br>
2019-03-11 18:35:03 -0300

    jenkins script

---
+++ jenkins_build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+DATE_DEPLOY=`TZ=America/Sao_Paulo date +'%d-%m-%Y %H:%M'`
+
+# VALIDACOES
+if [ -z "$GIT_COMMIT" ]; then
+	echo "GIT_COMMIT Vazio"
+	exit 1
+fi
+
+GIT_COMMIT=`echo $GIT_COMMIT | cut -c 1-7`
+GIT_TAG=`git describe --tags --abbrev=0 | sed 's/v//g'`
+
+sed -i -- "s/0.0.0/$GIT_TAG/g" index.html
+sed -i -- "s/##date##/$DATE_DEPLOY/g" index.html
+sed -i -- "s/##commit##/$GIT_COMMIT/g" index.html
