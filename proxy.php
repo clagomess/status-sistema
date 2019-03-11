899ffc10a403fefc27e742d63ea48c4dad44e22a
CLAUDIO GOMES <claudio.silva@fnde.gov.br>
2019-03-11 17:53:42 -0300

    rc

--- proxy.php
+++ proxy.php
@@ -2,6 +2,7 @@
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $_REQUEST['url']);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch,CURLOPT_TIMEOUT,1);
 
 $httpBody = curl_exec($ch);
 $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
@@ -9,5 +10,12 @@ $httpHeader = curl_getinfo($ch, CURLINFO_CONTENT_TYPE);
 
 curl_close($ch);
 
-header(sprintf("Content-Type: %s", $httpHeader), $httpCode);
+if(empty($httpBody) || $httpCode == 0){
+    $httpBody = '{"erro": "Falha ao conectar! Timeout 3s"}';
+    $httpCode = 500;
+    $httpHeader = 'application/json; charset=utf-8';
+}
+
+header(sprintf("Content-Type: %s", $httpHeader));
+http_response_code($httpCode);
 echo $httpBody;
